b75b6cfdcc4475088440f89f95051acd871ba1bc
acgtyrant <acgtyrant@gmail.com>
2020-05-19 23:40:50 +0800

    setup.py: upgrade to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="readmdict",
-    version="0.0.1",
+    version="0.0.2",
     packages=setuptools.find_packages(),
     install_requires=['python-lzo'],
 )
